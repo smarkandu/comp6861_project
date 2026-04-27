f096f1ec44943c931280b8123e3401e8c34cafff
smarkandu <s_markan@hotmail.com>
2026-04-26 22:09:30 -0400

    Bug Fix: Was missing get_speech_regions in SpeechBrainVAD

--- src/vad/SpeechBrainVAD.py
+++ src/vad/SpeechBrainVAD.py
@@ -1,6 +1,10 @@
-from vad.BaseVAD import BaseVAD
+from vad.BaseVAD import BaseVAD, TimeSpan
 from speechbrain.utils.fetching import LocalStrategy
 from speechbrain.inference.VAD import VAD
+import tempfile
+import numpy as np
+import soundfile as sf
+from typing import List
 
 class SpeechBrainVAD(BaseVAD):
     def __init__(
@@ -22,3 +26,11 @@ class SpeechBrainVAD(BaseVAD):
             savedir=savedir,
             local_strategy=LocalStrategy.COPY,
         )
+
+    def get_speech_regions(self, audio: np.ndarray, sr: int) -> List[TimeSpan]:
+        with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as tmp:
+            tmp_path = tmp.name
+
+        sf.write(tmp_path, audio, sr)
+
+        boundaries = self.vad.get_speech_segments(tmp_path)
